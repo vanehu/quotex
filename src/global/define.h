56819327ba133e436e1140243cc7cc8f0b9eb2de
xurendong <xrdlyj@163.com>
2018-03-31 11:05:36 +0800

    init project

---
+++ src/global/define.h
@@ -0,0 +1,39 @@
+﻿/*
+* Copyright (c) 2018-2018 the QuoteX authors
+* All rights reserved.
+*
+* The project sponsor and lead author is Xu Rendong.
+* E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
+* You can get more information at https://xurendong.github.io
+* For names of other contributors see the contributors file.
+*
+* Commercial use of this code in source and binary forms is
+* governed by a LGPL v3 license. You may get a copy from the
+* root directory. Or else you should get a specific written
+* permission from the project author.
+*
+* Individual and educational use of this code in source and
+* binary forms is governed by a 3-clause BSD license. You may
+* get a copy from the root directory. Certainly welcome you
+* to contribute code of all sorts.
+*
+* Be sure to retain the above copyright notice and conditions.
+*/
+
+#ifndef QUOTEX_GLOBAL_DEFINE_H
+#define QUOTEX_GLOBAL_DEFINE_H
+
+namespace quotex {
+
+// 软件信息
+#define DEF_APP_NAME "QuoteX" // 系统英文名称
+#define DEF_APP_NAME_CN "证 券 行 情 服 务 器" // 系统中文名称
+#define DEF_APP_VERSION "V0.1.0-Beta Build 20180331" // 系统版本号
+#define DEF_APP_DEVELOPER "Developed by the X-Lab." // 开发者声明
+#define DEF_APP_COMPANY "X-Lab (Shanghai) Co., Ltd." // 公司声明
+#define DEF_APP_COPYRIGHT "Copyright 2018-2018 X-Lab All Rights Reserved." // 版权声明
+#define DEF_APP_HOMEURL "http://www.xlab.com" // 主页链接
+
+} // namespace quotex
+
+#endif // QUOTEX_GLOBAL_DEFINE_H
--- src/readme.md
+++
@@ -1,2 +0,0 @@
-# QuoteX
-Quote Server
