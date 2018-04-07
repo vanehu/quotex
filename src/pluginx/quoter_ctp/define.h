ad4db57c2bc135c4749ea45287a0f06f4501cf92
xurendong <xrdlyj@163.com>
2018-04-07 14:07:10 +0800

    init plugin quoter_ctp

---
+++ src/pluginx/quoter_ctp/define.h
@@ -0,0 +1,38 @@
+/*
+* Copyright (c) 2018-2018 the QuoteX authors
+* All rights reserved.
+*
+* The project sponsor and lead author is Xu Rendong.
+* E-mail: xrd@ustc.edu, QQ: 277195007, WeChat: ustc_xrd
+* You can get more information at https://xurendong.github.io
+* For names of other contributors see the contributors file.
+*
+* Commercial use of this code in source and binary forms is
+* governed by a LGPL v3 license. You may get a copy from the
+* root directory. Or else you should get a specific written
+* permission from the project author.
+*
+* Individual and educational use of this code in source and
+* binary forms is governed by a 3-clause BSD license. You may
+* get a copy from the root directory. Certainly welcome you
+* to contribute code of all sorts.
+*
+* Be sure to retain the above copyright notice and conditions.
+*/
+
+#ifndef QUOTER_CTP_DEFINE_H
+#define QUOTER_CTP_DEFINE_H
+
+#include "../../global/define.h"
+
+// 插件定义
+#define PLUGIN_NAME_QUOTER_CTP "QuoterCTP" // 插件名称
+#define PLUGIN_NAME_CN_QUOTER_CTP "上 期 期 货 行 情 插 件" // 插件中文名称
+#define PLUGIN_VERSION_QUOTER_CTP "V0.1.0-Beta Build 20180407" // 插件版本号
+
+// 插件自定义消息
+
+// 插件自定义参数
+#define CFG_QUOTER_CTP_WORK_THREAD_NUM 1 // 输入输出服务工作线程数
+
+#endif // QUOTER_CTP_DEFINE_H
