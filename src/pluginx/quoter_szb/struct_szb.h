f15d4f03c8b7cc212ec4d37302bbcd00ca3de646
xurendong <xrdlyj@163.com>
2018-08-14 17:29:50 +0800

    improve quoter_szb

--- src/pluginx/quoter_szb/struct_szb.h
+++ src/pluginx/quoter_szb/struct_szb.h
@@ -141,6 +141,99 @@ public:
 	std::vector<T>* m_vec_cache_out;
 };
 
+struct MDGW_Header
+{
+	uint32_t m_MsgType; // 消息类型
+	uint32_t m_BodyLength; // 消息体长度
+};
+
+struct MDGW_Tailer
+{
+	uint32_t m_Checksum; // 校验和
+};
+
+struct MDGW_Logon
+{
+	MDGW_Header m_Header; // MsgType = 1
+	char m_SenderCompID[20]; // 发送方代码
+	char m_TargetCompID[20]; // 接收方代码
+	int32_t m_HeartBtInt; // 心跳间隔，单位是秒，用户行情系统登陆时提供给行情网关
+	char m_Password[16]; // 密码
+	char m_DefaultApplVerID[32]; // 二进制协议版本
+	MDGW_Tailer mdgw_tailer;
+};
+
+struct MDGW_Logon_Body
+{
+	char m_SenderCompID[20]; // 发送方代码
+	char m_TargetCompID[20]; // 接收方代码
+	int32_t m_HeartBtInt; // 心跳间隔，单位是秒，用户行情系统登陆时提供给行情网关
+	char m_Password[16]; // 密码
+	char m_DefaultApplVerID[32]; // 二进制协议版本
+};
+
+struct MDGW_Logout
+{
+	MDGW_Header m_Header; // MsgType = 2
+	int32_t m_SessionStatus; // 退出时的会话状态
+	char m_Text[200]; // 文本，注销原因的进一步补充说明
+	MDGW_Tailer mdgw_tailer;
+};
+
+struct MDGW_Logout_Body
+{
+	int32_t m_SessionStatus; // 退出时的会话状态
+	char m_Text[200]; // 文本，注销原因的进一步补充说明
+};
+
+struct MDGW_Heartbeat
+{
+	MDGW_Header m_Header; // MsgType = 3，BodyLength = 0
+	MDGW_Tailer mdgw_tailer;
+};
+
+struct MDGW_Heartbeat_Body
+{
+};
+
+#define DEF_MDGW_MSG_TYPE_LOGON 1
+#define DEF_MDGW_MSG_TYPE_LOGOUT 2
+#define DEF_MDGW_MSG_TYPE_HEARTBEAT 3
+
+uint32_t g_size_mdgw_header = sizeof( MDGW_Header );
+uint32_t g_size_mdgw_tailer = sizeof( MDGW_Tailer );
+uint32_t g_size_mdgw_logon_body = sizeof( MDGW_Logon_Body );
+uint32_t g_size_mdgw_logout_body = sizeof( MDGW_Logout_Body );
+uint32_t g_size_mdgw_heartbeat_body = sizeof( MDGW_Heartbeat_Body ); // 0
+
+uint32_t MDGW_GenerateCheckSum( char* buffer, uint32_t bufer_length ) {
+	uint32_t check_sum = 0;
+	for( size_t i = 0; i < bufer_length; ++i ) {
+		check_sum += (uint32_t)buffer[i];
+	}
+	return ( check_sum % 256 );
+}
+
+std::string MDGW_SessionStatus( int32_t status ) {
+	std::string result;
+	switch( status ) {
+	case 0: return result = "会话活跃";
+	case 1: return result = "会话口令已更改";
+	case 2: return result = "将过期的会话口令";
+	case 3: return result = "新会话口令不符合规范";
+	case 4: return result = "会话退登完成";
+	case 5: return result = "不合法的用户名或口令";
+	case 6: return result = "账户锁定";
+	case 7: return result = "当前时间不允许登录";
+	case 8: return result = "口令过期";
+	case 9: return result = "收到的 MsgSeqNum(34) 太小";
+	case 10: return result = "收到的 NextExpectedMsgSeqNum(789) 太大";
+	case 101: return result = "其他";
+	case 102: return result = "无效消息";
+	default: return result = "未知状态";
+	}
+}
+
 #ifdef SNAPSHOT_STOCK_SGT_V1
 struct SnapshotStock_SGT // 所有变量均会被赋值
 {
